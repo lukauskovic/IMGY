d858cec44c7529b2fe59a357420a8beeb6245d3c
lukauskovic <uskovicluka@gmail.com>
2017-11-25 20:03:40 +0100

    add tags and comments table/model

--- app/Image.php
+++ app/Image.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    public function comments()
+    {
+        return $this->hasMany('Comment', 'image_id');
+    }
+
+    public function tags(){
+        return $this->belongsToMany('Tag','images_tags','image_id');
+    }
 }
---
+++ app/Tag.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    public function images(){
+        return $this->belongsToMany('Image','images_tags','tag_id');
+    }
+}
